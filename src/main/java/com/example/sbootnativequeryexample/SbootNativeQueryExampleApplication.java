243d9ab32cf17e96e6d014b9107f60c986f5595b
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-08 17:25:43 -0400

    Creado repositorio, ejecución de querys nativas desde clase principal

--- src/main/java/com/example/sbootnativequeryexample/SbootNativeQueryExampleApplication.java
+++ src/main/java/com/example/sbootnativequeryexample/SbootNativeQueryExampleApplication.java
@@ -1,13 +1,30 @@
 package com.example.sbootnativequeryexample;
 
+import com.example.sbootnativequeryexample.entity.Tutorial;
+import com.example.sbootnativequeryexample.repository.TutorialRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbootNativeQueryExampleApplication {
+public class SbootNativeQueryExampleApplication implements CommandLineRunner {
+
+  @Autowired
+  TutorialRepository tutorialRepository;
 
   public static void main(String[] args) {
     SpringApplication.run(SbootNativeQueryExampleApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+    show(tutorials);
+  }
+
+  private void show(List<Tutorial> tutorials) {
+    tutorials.forEach(System.out::println);
+  }
 }
---
+++ src/main/java/com/example/sbootnativequeryexample/repository/TutorialRepository.java
@@ -0,0 +1,15 @@
+package com.example.sbootnativequeryexample.repository;
+
+import com.example.sbootnativequeryexample.entity.Tutorial;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+
+  @Query(value = "SELECT * FROM tutorials", nativeQuery = true)
+  List<Tutorial> findAll();
+
+  @Query(value = "SELECT * FROM tutorials t WHERE t.published = ?1", nativeQuery = true)
+  List<Tutorial> findByPublished(boolean isPublished);
+}
