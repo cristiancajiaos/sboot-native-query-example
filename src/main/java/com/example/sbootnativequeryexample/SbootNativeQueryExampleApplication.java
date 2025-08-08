81629dd7c487b5a4c8c059520fb65ab01286c1c7
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-08 16:46:19 -0400

    Primer commit

---
+++ src/main/java/com/example/sbootnativequeryexample/SbootNativeQueryExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbootnativequeryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootNativeQueryExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbootNativeQueryExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/sbootnativequeryexample/SbootNativeQueryExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbootnativequeryexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootNativeQueryExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
